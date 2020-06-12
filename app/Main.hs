30d8bc8e4bb978084d9d535e211160daf6cb6776
Adi Maini <adimaini@vt.edu>
2020-06-12 00:12:17 -0400

    Added functionality:
    1. Abilitiy to change resolution of the input image from app/Main.hs
    2. Ability to output image in that resolution to verify
    3. better handling of reading input image than before - reads in grayscale to begin with, rather than converting to grayscale afterwards.

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,10 @@ import ImageToVector
 
 main :: IO ()
 main = do
-    -- provide filepath and dimensions of new iage
-    to2DMatrix "images/test_image.jpg" "images/test_image_output.jpg" Edge (25, 25)
--- to2DMatrix "images/test_image.jpg" Bilinear 25 25
+    -- if you just want the 2D matrix output from the image
+    -- define the number of pixels (or dimensions) of the image you want
+    mat <- to2DMatrix "images/test_image.jpg" (100, 100)
+    writeFile ("images/test_image.txt") (show mat)
+
+    -- if you want to write an image to see the resolution, use this: 
+    toWriteImage "images/test_image.jpg" "images/test_image_output.jpg" (100, 100)
--- src/ImageToVector.hs
+++ src/ImageToVector.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 
 module ImageToVector (
-
+  to2DMatrix,
+  toWriteImage
 ) where
 
 import Codec.Picture         
@@ -14,39 +15,37 @@ import qualified Graphics.Image as I
 import Graphics.Image.IO
 import Graphics.Image.IO.Formats
 import Graphics.Image.Interface.Vector
--- import Graphics.Image.ColorSpace
+import Graphics.Image.ColorSpace
 import qualified Data.Map as Map
 import qualified Graphics.Image.Interface as Interface
 import Data.Word (Word8)
--- import Data.Vector (fromList)
 import qualified Data.Matrix as M
 import System.FilePath.Posix (splitExtension)
 
--- take image input with the filepath given as a DynamicImage type
-to2DMatrix :: FilePath -> FilePath -> Border(Interface.Pixel I.Y Word8) -> (Int, Int) -> IO ()
-to2DMatrix fp fpout bor (dim1, dim2)= do
+-- takes an image filepath and outputs a 2D matrix
+to2DMatrix :: FilePath  -> (Int, Int) -> IO (Maybe (M.Matrix Int)) 
+to2DMatrix fp (dim1, dim2)= do
     eimg <- I.readImageY VS fp 
     let new_res :: Interface.Image VS I.Y Word8
-        new_res = I.resize Bilinear bor (dim1, dim2) $ fmap conv eimg
+        new_res = I.resize Bilinear Edge  (dim1, dim2) $ Interface.map conv eimg
     let rle = twoDToMatrix $ pixelToInt $ toJPImageY8 new_res
+    return $ Just (rle)
+
+-- takes an input and output filepaths and writes an image on the given path
+-- using the similar processing as the 'to2DMarix' so you can see what the matrix looks like
+-- only difference is that the this function outputs with Pixels in Double precision
+-- whereas the 'to2DMatrix' function outputs in Word8 precision (0-255 value for each pixel)
+toWriteImage :: FilePath -> FilePath -> (Int, Int) -> IO () 
+toWriteImage fp fpout (dim1, dim2)= do
+    eimg <- I.readImageY VS fp 
+    let new_res :: Interface.Image VS I.Y Double
+        new_res = I.resize Bilinear Edge (dim1, dim2) eimg
     let (name, _) = splitExtension fp
-    writeFile (name ++ ".txt") (show rle)
-    -- I.writeImage fpout new_res
+    I.writeImage fpout new_res
 
--- convert image from Double to Word8
+-- convert image pixels from Double to Word8 using Functor
 conv :: Interface.Pixel I.Y Double -> Interface.Pixel I.Y Word8 
-conv d = floor $ fromIntegral d * 255
-
--- -- convert DynamicImage to a Pixel8 image
--- greyscaleImage :: DynamicImage -> Image Pixel8
--- greyscaleImage = convertRGB8 >>> pixelMap greyscalePixel
-
--- -- convert PixelsRGB8 image to Pixel8 image
--- greyscalePixel :: PixelRGB8 ->  Pixel8
--- greyscalePixel (PixelRGB8 r g b) = round (wr + wg + wb)
---   where wr = toRational r * (3 % 10)
---         wg = toRational g * (59 % 100)
---         wb = toRational b * (11 % 100)
+conv d = Interface.toWord8 <$> d
         
 -- convert Pixel8 image to a 2-d matrix of integers
 pixelToInt :: Image Pixel8 -> [[Int]]
@@ -61,23 +60,3 @@ pixelToInt =
 -- converts list of lists to Data.Matrix type Matrix
 twoDToMatrix :: [[Int]] -> M.Matrix Int
 twoDToMatrix lists = M.fromLists lists
-
-
-
---- draft code that tries to combine Graphics.Image and Codec.Picture by inputting, 
--- then outputting, then inputting an image. Does not work 
-
--- to2DMatrix :: FilePath -> IO() -> (Int, Int) -> Border(String) -> IO () -> IO()
--- to2DMatrix fp bor dim1 dim2 = do
---     image_rgb <- I.readImageRGB VU fp
---     case image_rgb of 
---       Left _ -> putStrLn $ "Sorry, not a supported codec for " ++ fp
---       Right dynimg -> do
---         let dimensions = (dim1, dim2)
---         new_image <- decodeImage $ I.resize bor dimensions dynimg
---         case new_image of
---           Left _ -> putStrLn $ "Sorry, not a supported codec for " ++ fp
---           Right image -> do
---             let rle = twoDToMatrix $ pixelToInt $ greyscaleImage image
---             let (name, _) = splitExtension fp
---             writeFile (name ++ ".txt") (show rle)
