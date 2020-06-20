69ea6680f63e54a60b4b391e3c75ab010c52e97e
Adi Maini <adimaini@vt.edu>
2020-06-20 17:38:55 -0400

    Changes made:
    1. make a column vector of each image. Return type is Vector instead of Matrix.
    2. added MNIST_data paths, chunk size
    3. ability to transverse all files in a directory and return list of all filepaths.
    4. Regex to detect the label or class by the subfolder sub string.
    5. Shuffling lists to shuffle the classes or labels

--- app/Main.hs
+++ app/Main.hs
@@ -9,8 +9,17 @@ main = do
 
     -- if you just want the 2D matrix output from the image
     -- define the number of pixels (or dimensions) of the image you want
-    mat <- to2DMatrix "images/test_image.jpg" (100, 100)
-    writeFile "images/test_image.txt" (show mat)
+    -- mat <- to2DMatrix "images/test_image.jpg" (100, 100)
+    -- writeFile "images/test_image.txt" (show mat)
 
     -- if you want to writesee the resolution, use this: 
-    toWriteImage "images/test_image.jpg" "images/test_image_output.jpg" (100, 100)
+    -- toWriteImage "images/test_image.jpg" "images/test_image_output.jpg" (100, 100)
+
+    let train_filepath = 'MNIST_data/training'
+    let chunk_size = 10
+
+    list_fp <- traverseDir train_filepath "None"
+    shuff_list <- take chunk_size $ shuffle list_fp
+
+    train_image = do
+        vectors = fmap labelAndTrain shuff_list 
