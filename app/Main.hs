2294a8c6c221d9022955321fc9a124e437917049
ANekhai <30994106+ANekhai@users.noreply.github.com>
2020-06-21 00:05:51 -0400

    Add files via upload

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,8 @@ module Main where
 
 
 import ImageToVector
-import System.FilePath.Posix (splitExtension)
+import System.Directory
+import System.FilePath.Posix (isPathSeparator, (</>))
 
 main :: IO ()
 main = do
@@ -15,11 +16,29 @@ main = do
     -- if you want to writesee the resolution, use this: 
     -- toWriteImage "images/test_image.jpg" "images/test_image_output.jpg" (100, 100)
 
-    let train_filepath = 'MNIST_data/training'
-    let chunk_size = 10
+    let train_filepath = "MNIST_data/training"
+        chunk_size = 10
 
-    list_fp <- traverseDir train_filepath "None"
-    shuff_list <- take chunk_size $ shuffle list_fp
+    sub_files <- listDirectory train_filepath -- should be a list ['0', '1', '2' ... '9']
+    let 
+        subdirs = map (train_filepath </> ) sub_files
+        labelled_subdirs = zipWith (,) labels subdirs
+        
+        
 
-    train_image = do
-        vectors = fmap labelAndTrain shuff_list 
+
+    --list_fp <- traverseDir train_filepath (=="None")
+--    shuffled_list <- shuffle list_fp
+
+    return ()
+
+    -- train_image = do
+    --     vectors = fmap labelAndTrain shuffled_list 
+
+    
+-- shuffles a list
+shuffle :: [a] -> IO [a]
+shuffle [] = return []
+shuffle xs = do randomPosition <- getStdRandom (randomR (0, length xs - 1))
+                let (left, (a:right)) = splitAt randomPosition xs
+                fmap (a:) (shuffle (left ++ right))
